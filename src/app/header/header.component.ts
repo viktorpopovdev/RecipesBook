1d314a563d53b97f479906f52146a502705df7d2
Viktor <viktor@Viktor-MacBook-Pro.local>
2024-02-29 17:28:57 +0200

    Added navigation for Header, done somr changes in recipe-item

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  @Output() featureSelected = new EventEmitter<string>();
+  onSelect(feature: string) {
+    if (feature === 'recipe') {
+      this.featureSelected.emit(feature);
+    } else {
+      this.featureSelected.emit(feature);
+    }
+  }
 }
